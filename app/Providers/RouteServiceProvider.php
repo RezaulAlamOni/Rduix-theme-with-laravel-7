2e820d0452a894ef278418d6071068cd58e55de4
Shahinur Rahman <coredeveloper.2013@gmail.com>
2020-03-10 16:27:51 +0600

    create new route separated route file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapWebRoutesCopy();
+
         //
     }
 
@@ -61,6 +63,12 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
+    }
+    protected function mapWebRoutesCopy()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('app/Http/router/web.php'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,3 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
